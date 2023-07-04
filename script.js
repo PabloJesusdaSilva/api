bff9d56deaaa1ca8ede53d40578c1262bfa8ca9f
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2023-07-04 16:57:24 -0300

    Criando requisição para atualizar produtos

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const productDiv = document.querySelector('#product');
+/*const productDiv = document.querySelector('#product');
 const btn = document.querySelector('#btn');
 const req = new Request('http://localhost:3000/produtos', {
     method:'GET',
@@ -8,24 +8,78 @@ const req = new Request('http://localhost:3000/produtos', {
 });
 
 
-btn.addEventListener('mousedown', () => {
+btn.addEventListener('click', () => {
     fetch(req)
     .then(res => res.json())
     .then(res => {
         productDiv.innerHTML = ''
         
-         for(let i = 0; i < res.length; i++) {
-
-             const product = `
-             <li>ID: ${res[i].id}</li>
+        for(let i = 0; i < res.length; i++) {
+            
+            const product = `
+            <li>ID: ${res[i].id}</li>
              <li>DESCRIÇÃO: ${res[i].descricao}</li>
              <li>PREÇO: ${res[i].preco}</li>
              `;
              
-            productDiv.appendChild(ul).innerHTML = product;
-        }
-        
+             productDiv.appendChild(ul).innerHTML = product;
+            }
+            
+        });
     });
+    
+    */
+const btnATualizar = document.querySelector('#btnAtuazlizar');
+
+btnATualizar.addEventListener('click', () => {
+    const inputId = document.querySelector('#id').value;
+    const inputDescricao = document.querySelector('#descricao').value;
+    const inputPreco = document.querySelector('#preco').value;
+    const inputImagem = document.querySelector('#imagem').value;
+
+    const dados = {
+        id: null,
+        descricao: inputDescricao,
+        preco: inputPreco,
+        imagem: inputImagem
+    }
+
+    fetch(`http://localhost:3000/produtos/${inputId}`, {
+        method: 'PUT',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(dados)
+    })
+        .then(res => {
+            console.log(res.status)
+        })
+
+        getProdutos()
 });
 
-console.log('teste')
+function getProdutos() {
+    const productDiv = document.querySelector('#product');
+    
+    fetch('http://localhost:3000/produtos', {
+        method:'GET',
+        headers: {
+            'Content-type': 'application/json'
+            }   
+        })
+        .then(res => res.json())
+        .then(res => {
+            productDiv.innerHTML = ''
+            
+            for(let i = 0; i < res.length; i++) {
+                
+                const product = `
+                <li>ID: ${res[i].id}</li>
+                <li>DESCRIÇÃO: ${res[i].descricao}</li>
+                <li>PREÇO: ${res[i].preco}</li>
+                `;
+                
+                productDiv.appendChild(ul).innerHTML = product;
+                }
+        });
+}
