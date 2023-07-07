642e79e94bc2061361f7460fd0add95f6d3a7529
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2023-07-07 17:00:53 -0300

    Fix: corrigindo erros de sintaxe

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,35 +4,42 @@ linkStyles.setAttribute('href', './assets/css/style.css');
 
 document.head.appendChild(linkStyles);
 
-// Exibindo produtos
+function getProdutos(botoes = false) {    
 
-const productDiv = document.querySelector('#product');
-
-document.querySelector('#btn-fetch').addEventListener('click', () => {
-    fetch('http://localhost:3000/produtos', {
-        method:'GET',
-        headers: {
-            'Content-type': 'application/json'
-        }
+fetch('http://localhost:3000/produtos', {
+    method:'GET',
+    headers: {
+        'Content-type': 'application/json'
+        }   
     })
 
-    .then(res => res.json())
-    .then(res => {
-        productDiv.innerHTML = '';
-        
-        for (let i = 0; i < res.length; i++) {
-            const ul = document.createElement("ul");
-
-            const product = `
-                <li>ID: ${res[i].id}</li>
-                <li>DESCRIÇÃO: ${res[i].descricao}</li>
-                <li>PREÇO: ${res[i].preco}</li>
-                `;
+        .then(res => res.json())
+        .then(res => {
+            document.querySelector('#listaProdutos').innerHTML = "";
             
-            productDiv.appendChild(ul).innerHTML = product;
-        }      
-    });
-});
+            for (let i = 0; i < res.length; i++) {
 
+                const ul = document.createElement('ul');
+                ul.classList.add('produto');
+                const img = document.createElement('img');
 
-// Criação de produtos
+                ul.appendChild(document.createElement('li')).innerHTML = res[i].id;
+                ul.appendChild(document.createElement('li')).innerHTML = res[i].descricao;
+                ul.appendChild(document.createElement('li')).innerHTML = res[i].preco;
+                ul.appendChild(document.createElement('li')).appendChild(img).setAttribute('src', res[i].imagem);
+
+                if (botoes) {
+                    const btnDelete = document.createElement('button');
+                    
+                    btnDelete.setAttribute('type', 'button');
+                    btnDelete.classList.add('btnDelete');
+                    btnDelete.value = res[i].id;
+                    btnDelete.innerHTML = 'X';
+
+                    ul.appendChild(document.createElement('li')).appendChild(btnDelete);
+                }
+
+                document.querySelector('#listaProdutos').appendChild(ul);
+            }
+        });
+}
