222af3119e868e119d10956ea03af6357a0009f2
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2023-07-10 16:54:42 -0300

    Corrigindo erros

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,42 +4,50 @@ linkStyles.setAttribute('href', './assets/css/style.css');
 
 document.head.appendChild(linkStyles);
 
-function getProdutos(botoes = false) {    
+function getProdutos(btnDelete = false) {    
 
-fetch('http://localhost:3000/produtos', {
-    method:'GET',
-    headers: {
-        'Content-type': 'application/json'
-        }   
-    })
+    fetch('http://localhost:3000/produtos', {
+        method:'GET',
+        headers: {
+            'Content-type': 'application/json'
+            }   
+        })
 
-        .then(res => res.json())
-        .then(res => {
-            document.querySelector('#listaProdutos').innerHTML = "";
-            
-            for (let i = 0; i < res.length; i++) {
+            .then(res => res.json())
+            .then(res => {
+                let listaProdutos = document.querySelector('#listaProdutos');
+                listaProdutos.innerHTML = "";
+                
+                for (let i = 0; i < res.length; i++) {
 
-                const ul = document.createElement('ul');
-                ul.classList.add('produto');
-                const img = document.createElement('img');
+                    const ul = document.createElement('ul');
+                    ul.classList.add('produto');
 
-                ul.appendChild(document.createElement('li')).innerHTML = res[i].id;
-                ul.appendChild(document.createElement('li')).innerHTML = res[i].descricao;
-                ul.appendChild(document.createElement('li')).innerHTML = res[i].preco;
-                ul.appendChild(document.createElement('li')).appendChild(img).setAttribute('src', res[i].imagem);
+                   
 
-                if (botoes) {
-                    const btnDelete = document.createElement('button');
-                    
-                    btnDelete.setAttribute('type', 'button');
-                    btnDelete.classList.add('btnDelete');
-                    btnDelete.value = res[i].id;
-                    btnDelete.innerHTML = 'X';
+                    const img = document.createElement('img');
 
-                    ul.appendChild(document.createElement('li')).appendChild(btnDelete);
+                    if (btnDelete) {
+                        const liBtn = document.createElement('li');
+
+                        const btn = document.createElement('button');
+                        btn.type = 'button';
+                        btn.innerHTML = 'X';
+                        btn.classList.add('btn-delete');
+                        btn.value = res[i].id;
+
+                        ul.appendChild(liBtn).appendChild(btn);
+                    }
+
+                    ul.appendChild(document.createElement('li')).innerHTML = res[i].id;
+                    ul.appendChild(document.createElement('li')).innerHTML = res[i].descricao;
+                    ul.appendChild(document.createElement('li')).innerHTML = res[i].preco;
+                    ul.appendChild(document.createElement('img')).appendChild(img).setAttribute('src', res[i].imagem);
+
+                    document.querySelector('#listaProdutos').appendChild(ul);
                 }
+            });
 
-                document.querySelector('#listaProdutos').appendChild(ul);
-            }
-        });
 }
+
+    
